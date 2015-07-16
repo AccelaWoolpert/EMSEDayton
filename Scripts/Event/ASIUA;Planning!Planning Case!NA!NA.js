9bc67cb87af83c31d29cccc33507eb32b0ae3537
AccelaWoolpert <Deanna.Hoops@woolpert.com>
2015-07-16 10:17:14 -0400

    note

--- Scripts/Event/ASIUA;Planning!Planning Case!NA!NA.js
+++ Scripts/Event/ASIUA;Planning!Planning Case!NA!NA.js
@@ -2,7 +2,7 @@
 try {
 	if ( matches(AInfo["Change Case Type"],"Y","Yes") ){
 		editAppName(""+AInfo["Case Type"])
-		editAppSpecific("Change Case Type","N")
+		editAppSpecific("Change Case Type","N") //Need to verify if this is needed
 	}
 }
 catch(err){
