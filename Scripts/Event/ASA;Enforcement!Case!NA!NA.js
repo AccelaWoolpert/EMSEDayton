ba96562f07d084e4bd8818cacf4ecf44248b93b2
Christopher <cisensano@yahoo.com>
2016-10-05 11:05:55 -0700

    removed code from line 60+
        function getScriptText(vScriptName) {
            vScriptName = vScriptName.toUpperCase();
            var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
            var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
            return emseScript.getScriptText() + "";
        }
        //****************************************************************
        //  Accela Script include
        //****************************************************************
        eval(getScriptText("INCLUDES_CUSTOM"));
        eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
    
        //Global settings...
        var showMessage = true;                        // Set to true to see results in popup window
        var showDebug = true;                            // Set to true to see debug messages in popup window
        var message = "";                            // Message String
        var debug = "";                                // Debug String
        var br = "<BR>";                            // Break Tag

--- Scripts/Event/ASA;Enforcement!Case!NA!NA.js
+++ Scripts/Event/ASA;Enforcement!Case!NA!NA.js
@@ -57,24 +57,7 @@ scheduleInspectDate("Complaint",dateAdd(null,numDays),assignedStaff);
 try {
     logDebug("Begin Accela > Hansen Test Script");
 
-    function getScriptText(vScriptName) {
-        vScriptName = vScriptName.toUpperCase();
-        var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
-        var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
-        return emseScript.getScriptText() + "";
-    }
-    //****************************************************************
-    //  Accela Script include
-    //****************************************************************
-    eval(getScriptText("INCLUDES_CUSTOM"));
-    eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
 
-    //Global settings...
-    var showMessage = true;                        // Set to true to see results in popup window
-    var showDebug = true;                            // Set to true to see debug messages in popup window
-    var message = "";                            // Message String
-    var debug = "";                                // Debug String
-    var br = "<BR>";                            // Break Tag 
 
     //****************************************************************
     //Custom Web Service settings
