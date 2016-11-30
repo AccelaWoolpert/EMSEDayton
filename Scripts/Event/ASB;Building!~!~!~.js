025696ba7c166a162a349daffc78a94827840382
SYSTEM <SYSTEM@ctyaas2-d.cty.daytonohio.gov>
2016-11-30 12:13:32 -0500

    commit by emse tools 2016-11-30 12:13:32

--- Scripts/Event/ASB;Building!~!~!~.js
+++ Scripts/Event/ASB;Building!~!~!~.js
@@ -1,11 +1,11 @@
 //Script 5
-if (matches(appTypeArray[1],"Gas","Electrical","Mechanical","Plumbing","Sewer","Water") && matches(appTypeArray[2],"Residential")) {
+/*if (matches(appTypeArray[1],"Gas","Electrical","Mechanical","Plumbing","Sewer","Water") && matches(appTypeArray[2],"Residential")) {
 	if (!matches(ApplicantContactType,"Owner","Trade Contractor")){
 		showMessage = true
 		cancel = true
 		logMessage("Applicant must be an 'Owner' or a 'Trade Contractor'")
 	}
-}
+}*/
 
 //Script 6
 if (matches(appTypeArray[1],"Mechanical")) {
