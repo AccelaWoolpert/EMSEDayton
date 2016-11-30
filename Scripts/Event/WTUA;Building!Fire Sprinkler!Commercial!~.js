97fc22e3ae0a613da6e39e8343e163fe99b17eda
Christopher <cisensano@yahoo.com>
2016-11-30 09:31:05 -0800

    added fee "FIR_090" per UAT item 10

--- Scripts/Event/WTUA;Building!Fire Sprinkler!Commercial!~.js
+++ Scripts/Event/WTUA;Building!Fire Sprinkler!Commercial!~.js
@@ -2,5 +2,6 @@
 if(wfTask == "Application Submittal" && wfStatus == "Accepted" && AInfo["Job Cost"] > 0){
 	updateFee("FIR_010","B_FIRE","FINAL",AInfo["Job Cost"],"N");
 	updateFee("FIR_020","B_FIRE","FINAL",1,"N");
+	updateFee("FIR_090","B_FIRE","FINAL",1,"N");
 }
 
