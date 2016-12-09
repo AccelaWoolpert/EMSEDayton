83c1264a932b9780d7086d99e79b50f0409b3f41
Christopher <cisensano@yahoo.com>
2016-12-09 10:02:19 -0800

    Added fee and update fee order

--- Scripts/Event/WTUA;Building!Fire Alarm!Commercial!~.js
+++ Scripts/Event/WTUA;Building!Fire Alarm!Commercial!~.js
@@ -1,7 +1,8 @@
 //assess fees
 if(wfTask == "Application Submittal" && wfStatus == "Accepted" && AInfo["Job Cost"] > 0){
-	updateFee("FIR_010","B_FIRE","FINAL",AInfo["Job Cost"],"N");
 	updateFee("FIR_090","B_FIRE","FINAL",1,"N");
+	updateFee("FIR_022","B_FIRE","FINAL",1,"N");
+	updateFee("FIR_010","B_FIRE","FINAL",AInfo["Job Cost"],"N");
 	updateFee("FIR_020","B_FIRE","FINAL",1,"N");
 }
 
