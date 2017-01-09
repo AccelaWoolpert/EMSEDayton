cfcc6213c4d3e4d107c0710055808aacf4a844e4
Christopher <cisensano@yahoo.com>
2017-01-08 20:37:00 -0800

    Updated script 11 to set Plan Review Expiration Date instead of Permit Expiration Date

--- Scripts/Event/WTUA;Building!~!~!~.js
+++ Scripts/Event/WTUA;Building!~!~!~.js
@@ -1,16 +1,12 @@
 //Script 11
 if (matches(wfTask,"Application Submittal","Application Submitted") && wfStatus=="Accepted") {
-	today = new Date()
-	editAppSpecific("Application Date",jsDateToASIDate(today))
-	today.setMonth(today.getMonth()+6)
-	editAppSpecific("Permit Expiration Date",jsDateToASIDate(today))
+	editAppSpecific("Application Date",dateAdd(null,0));
+	editAppSpecific("Plan Review Expiration Date",dateAdd(null,180));
 }
 
 //Script 12
 if (wfTask.slice(-6) == "Review") {
-	expDate = new Date()
-	expDate.setMonth(expDate.getMonth()+6)
-	editAppSpecific("Plan Review Expiration Date",jsDateToASIDate(expDate))
+	editAppSpecific("Plan Review Expiration Date",dateAdd(null,180))
 }
 
 //Script 14
