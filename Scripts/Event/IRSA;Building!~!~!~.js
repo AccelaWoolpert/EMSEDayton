8641d45454d743b9824c9c87261d34a9bd152ddf
Christopher <cisensano@yahoo.com>
2017-05-08 11:20:40 -0700

    Added app status updates

--- Scripts/Event/IRSA;Building!~!~!~.js
+++ Scripts/Event/IRSA;Building!~!~!~.js
@@ -7,10 +7,12 @@ editAppSpecific("Permit Expiration Date",jsDateToASIDate(expDate))
 if (matches(inspType, "Final w/ CUO", "Final w/o CUO") && inspResult == "Approved") {
 	closeTask("Inspection","Complete","Updated via Script","Updated via Script")
 	closeTask("Inspections","Complete","Updated via Script","Updated via Script")
+	updateAppStatus("Inspected","Updated via Script");
 	
 	//Script 18
 	if (matches(""+getAppSpecific("CUO Required"),"N","No")) {
 		closeOutWorkflow("Closed","Updated via Script")
+		updateAppStatus("Final","Updated via Script");
 	}
 		
 }
