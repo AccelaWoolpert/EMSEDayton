fe3893c451e7c08bd5fd8dfa3396a3f3c65aa3f6
Christopher <cisensano@yahoo.com>
2017-05-09 12:03:29 -0700

    Added Final status for MEP's

--- Scripts/Event/IRSA;Building!~!~!~.js
+++ Scripts/Event/IRSA;Building!~!~!~.js
@@ -4,7 +4,7 @@ expDate.setMonth(expDate.getMonth()+12)
 editAppSpecific("Permit Expiration Date",jsDateToASIDate(expDate))
 
 //Script 16
-if (matches(inspType, "Final w/ CUO", "Final w/o CUO") && inspResult == "Approved") {
+if (matches(inspType, "Final w/ CUO", "Final w/o CUO", "Final") && inspResult == "Approved") {
 	closeTask("Inspection","Complete","Updated via Script","Updated via Script")
 	closeTask("Inspections","Complete","Updated via Script","Updated via Script")
 	updateAppStatus("Inspected","Updated via Script");
