b45f52936398cd66b1a25dfd8412e46c8b94526c
Christopher <cisensano@yahoo.com>
2017-05-09 11:34:36 -0700

    these records are not used and are disabled. The scripts were configured for com fees and would need to be developed for the res fees if those records are enabled in the future.

--- Scripts/Event/WTUA;Building!Fire Sprinkler!Residential!~.js
+++
@@ -1,8 +0,0 @@
-//assess fees
-if(wfTask == "Application Submittal" && wfStatus == "Accepted" && AInfo["Job Cost"] > 0){
-	updateFee("FIR_010","B_FIRE","FINAL",AInfo["Job Cost"],"N");
-	updateFee("FIR_090","B_FIRE","FINAL",1,"N");
-	updateFee("FIR_020","B_FIRE","FINAL",1,"N");
-	updateFee("FIR_022","B_FIRE","FINAL",1,"N");
-}
-
